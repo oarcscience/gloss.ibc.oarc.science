8af908f086dec1f346a9e3e05bd4305f76da0538
- <->
2020-06-21 10:10:50 +0200

    edit

--- w/genStrong.py
+++ w/genStrong.py
@@ -63,7 +63,7 @@ for i  in range(df.shape[0]):
 <p style="margin: 0px;background-color: #7296cc;padding: 5px;width:27%;"></p>
 
 """+"""<h1>{} {} ‘{}’ <span style="color: rgba(0, 0, 0, 0.75);font-size: 65%; font-family: EB Garamond;font-weight: normal;">({})</span></h1>""".format(df.iloc[i,22], df.iloc[i,2], df.iloc[i,20], lang_full(df.iloc[i,3]))+"""
-<p>Etymology: {}""".format(df.iloc[i,15])+wrapper(""" | <span style="color: rgba(0, 0, 0, 0.75)">Roots:</span> {} """, wrapper2("""
+<p><span style="color: rgba(0, 0, 0, 0.75)">Etymology:</span> {}""".format(df.iloc[i,15])+wrapper(""" | <span style="color: rgba(0, 0, 0, 0.75)">Roots:</span> {} """, wrapper2("""
 <a class="shadow" href="/w/{}">{}</a>""", stn(df.iloc[i,8] ), df.iloc[i,9])+wrapper2("""
 <a class="shadow" href="/w/{}">{}</a>""", stn(df.iloc[i,10]), df.iloc[i,11])+wrapper2("""
 <a class="shadow" href="/w/{}">{}</a>""", stn(df.iloc[i,12]), df.iloc[i,13]))+wrapper("""
