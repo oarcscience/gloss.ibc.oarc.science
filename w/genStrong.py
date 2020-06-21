54519745b5bf3a0cf6aec764dbcf87e76fab5d3c
- <->
2020-06-21 02:09:17 +0200

    edit

--- w/genStrong.py
+++ w/genStrong.py
@@ -11,6 +11,14 @@ print('Loading verses...\r',end="")
 df=pd.read_csv("../_data/TBESH_formatted.csv",sep="\t").fillna("")
 
 
+def wrapper(line, arg):
+    if arg=="":return ""
+    else: return line.format(arg)
+
+def wrapper2(line, arg1, arg2):
+    if arg2=="":return ""
+    else: return line.format(arg1, arg2)
+
 
 for i  in range(df.shape[0]):
     if i%100==0:
@@ -55,16 +63,12 @@ for i  in range(df.shape[0]):
 <p style="margin: 0px;background-color: #7296cc;padding: 5px;width:27%;"></p>
 
 """+"""<h1>{} {} ‘{}’ <span style="color: rgba(0, 0, 0, 0.75);font-size: 65%; font-family: EB Garamond;font-weight: normal;">({})</span></h1>""".format(df.iloc[i,22], df.iloc[i,2], df.iloc[i,20], lang_full(df.iloc[i,3]))+"""
-<p>Etymology: {}</p>""".format(df.iloc[i,15])+"""
-<p>Roots: <a class="shadow" href="/w/{}">{}</a> <a class="shadow" href="/w/{}">{}</a> <a class="shadow" href="/w/{}">{}</a></p>""".format(stn(df.iloc[i,8]), df.iloc[i,9], stn(df.iloc[i,10]), df.iloc[i,11], stn(df.iloc[i,12]), df.iloc[i,13])+"""   
-<p>frequency: {}, first occurence (encoded): {}</p>""".format(stn(df.iloc[i,4]), stn(df.iloc[i,5]))+"""
-<p>In another language: <strong>{}</strong> ({})</p>
-<p>Morphology: {}</p>
-<p>Roots: {}</p>
-<p>Variants: {}</p>
-<p>Morphology: {}</p>
-<p>Meaning: {}</p>
-""".format(*[df.iloc[i,num] for num in [6,7,14,16,18,19,21]])+"""
+<p>Etymology: {}</p>""".format(df.iloc[i,15])+wrapper("""<p>Roots: {}</p>""", wrapper2("""
+<a class="shadow" href="/w/{}">{}</a>""", stn(df.iloc[i,8] ), df.iloc[i,9])+wrapper2("""
+<a class="shadow" href="/w/{}">{}</a>""", stn(df.iloc[i,10]), df.iloc[i,11])+wrapper2("""
+<a class="shadow" href="/w/{}">{}</a>""", stn(df.iloc[i,12]), df.iloc[i,13]))+wrapper("""
+<p>In another language: {}</p>""", wrapper("""<strong>{}</strong> """, df.iloc[i,6]))+wrapper("""({})""", df.iloc[i,7])+wrapper("""
+<p>Variants: {}</p>""", df.iloc[i,19])+wrapper("""<p>Meaning<br> {}</p>""", df.iloc[i,21])+"""
 
 </article>
 
