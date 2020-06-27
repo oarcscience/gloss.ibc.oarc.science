d487a3664d77aa006aa961ac4c1f980441d1f8ed
- <->
2020-06-27 18:56:26 +0200

    edit

--- translatoraid/sources/MorphologyTranslate.py
+++ translatoraid/sources/MorphologyTranslate.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 def deletenull(l):
-    return [o if o!="" for o in l]
+    return [o for o in l if o!="" ]
 
 import pandas as pd
 master = pd.read_csv("mlexiconmaster.csv",sep="\t")
