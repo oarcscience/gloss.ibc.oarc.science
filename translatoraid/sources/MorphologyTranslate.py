ce87b7d9551a4256ffdaac0ea430d4e0a5a0d843
- <->
2020-06-27 18:09:18 +0200

    edit

--- translatoraid/sources/MorphologyTranslate.py
+++ translatoraid/sources/MorphologyTranslate.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+def deletenull(l):
+    return [o if o!="" for o in l]
+
 import pandas as pd
 master = pd.read_csv("mlexiconmaster.csv",sep="\t")
 dictio = pd.read_csv("morpholex.csv",index_col="full").to_dict()["short"]
 splitcomma  = lambda x: [t.strip() for t in x.split(",")]
 applydictio = lambda x: list(map(dictio.get, x))
-master["value"]=master["morphologyDetail"].apply(splitcomma).apply(applydictio).apply(", ".join)
+master["value"]=master["morphologyDetail"].apply(splitcomma).apply(deletenull).apply(applydictio).apply(", ".join)
 master[["key","value"]].to_csv("mlexicon.csv",index=False)
