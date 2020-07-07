72d411ec9dc0a412378e4a1a090b9a01a17de57e
- <->
2020-07-08 01:38:49 +0200

    edit

--- w/genStrong.py
+++ w/genStrong.py
@@ -20,6 +20,15 @@ df=pd.read_csv("../_data/TBESH_formatted.csv",sep="\t").fillna("")
 print('Loading indexing information...\r',end="")    
 index=pd.read_csv("../_data/temp.csv",sep="\t")
 
+def plur_linear(s):
+    lis=s.split("|")
+    if len(lis)==1: return s
+    return " ".join(["""<sup style="color:gray;">{}</sup>""".format(str(i))+word for i,word in zip(range(1,len(lis)+1),lis)])
+        
+def plur_vertical(s):
+    lis=s.split("|")
+    if len(lis)==1: return s
+    return "\n".join(["""<strong>{}</sup> """.format(chr(ord('A')-1+i))+word for i,word in zip(range(1,len(lis)+1),lis)])
 
 
 def wrapper(line, arg):
@@ -109,15 +118,15 @@ for i  in range(df.shape[0]):
 <p style="margin: 0px;background-color: #ff7f2a;padding: 5px;width:38%;"></p>
 <p style="margin: 0px;background-color: #7296cc;padding: 5px;width:27%;"></p>
 
-"""+"""<h1>{} <span id="bh">{}</span> ‘{}’ <span style="color: rgba(0, 0, 0, 0.75);font-size: 65%; font-family: EB Garamond;font-weight: normal;">({})</span></h1>""".format(df.iloc[i,22], df.iloc[i,2], df.iloc[i,20], lang_full(df.iloc[i,3]))+"""
+"""+"""<h1>{} <span id="bh">{}</span> ‘{}’ <span style="color: rgba(0, 0, 0, 0.75);font-size: 65%; font-family: EB Garamond;font-weight: normal;">({})</span></h1>""".format(df.iloc[i,22], df.iloc[i,2], plur_linear(df.iloc[i,20]), lang_full(df.iloc[i,3]))+"""
 <p><span style="color: rgba(0, 0, 0, 0.75)">Etymology:</span> {}""".format(df.iloc[i,15])+wrapper(""" | <span style="color: rgba(0, 0, 0, 0.75)">Roots:</span> {} """, wrapper2("""
 <a class="shadow" href="/w/{}"><span id="bh">{}</span></a>""", stn(df.iloc[i,8] ), df.iloc[i,9])+wrapper2("""
 <a class="shadow" href="/w/{}"><span id="bh">{}</span></a>""", stn(df.iloc[i,10]), df.iloc[i,11])+wrapper2("""
 <a class="shadow" href="/w/{}"><span id="bh">{}</span></a>""", stn(df.iloc[i,12]), df.iloc[i,13]))+wrapper("""
  | In another language: {}  
 """, wrapper("""<strong><span id="bh">{}</span></strong> """, df.iloc[i,6]))+wrapper("""({})""", stn(df.iloc[i,7]))+wrapper(""" | <span style="color: rgba(0, 0, 0, 0.75)">Variants:</span> {}
-""", df.iloc[i,18])+wrapper(""" | <span style="color: rgba(0, 0, 0, 0.75)">Morphology:</span> {} """, df.iloc[i,19])+wrapper("""</p><p>{}</p>
-""", df.iloc[i,21])+"""
+""", df.iloc[i,18])+wrapper(""" | <span style="color: rgba(0, 0, 0, 0.75)">Morphology:</span> {} """, plur_linear(df.iloc[i,19]))+wrapper("""</p><p>{}</p>
+""", plur_vertical(df.iloc[i,21]))+"""
 
 </article>
 
