eafd4c0101f863544791e20604efbd963fdb9425
- <>
2021-09-04 19:19:42 +0200

    edit

--- generateVerses.py
+++ generateVerses.py
@@ -35,7 +35,15 @@ def repl(inputw):
 import pandas as pd
 print('Loading verses...\r',end="")    
 df=pd.read_csv("_data/bible.csv",sep="\t")
+## df : hebtext	number	book	chapter	verse	trchapter	trverse	trtext	translit
+##      0       1       2       3       4       5           6       7       8
+
 ixv=pd.read_csv("_data/indexv.csv",sep="\t",header=None)
+ixv.columns=["previous_verse","previous_book", "previous_chapter", "next_verse", "next_book", "next_chapter"]
+## ixv : index file containing navigation. Format
+## 1	1	1	2	1534	32
+## previous_verse previous_book previous_chapter next_verse next_book next_chapter
+## 0              1             2                3          4         5
 
 
 print('Loading words...\r',end="")    
@@ -69,11 +77,11 @@ print("Generating syntax info...")
 phrasesjoinedt=joinedt.apply(generateqtree,axis=1).groupby(["WLCverse","curr"]).apply("\n".join)
 
 
-templatec="""<div class=wrapper><p style="font-family: sans-serif;font-size: 125%;">{}: {}</p><p>{}</p></div>"""
+templateclause="""<div class=wrapper><p style="font-family: sans-serif;font-size: 125%;">{}: {}</p><p>{}</p></div>"""
 
 
 def generateqtree2(s):
-    return templatec.format(s["ckind"],s["clausefunction"],s["phrasesjoined"])
+    return templateclause.format(s["ckind"],s["clausefunction"],s["phrasesjoined"])
 
 print("Generating syntax info 2 ...")
 
@@ -97,15 +105,23 @@ for i  in range(df.shape[0]):
     if i%100==0:
         print('Generating verses: {:.0%}'.format(i/df.shape[0]),"\r",end="")    
    
-    with open("v/"+books[df.iloc[i,2]-1].replace(" ","")+"."+str(df.iloc[i,3])+"."+str(df.iloc[i,4])+".html","w+") as fout:
-        fout.write(verseTemplate.format(books[df.iloc[i,2]-1], df.iloc[i,3], df.iloc[i,4], df.iloc[i,7], ixv.iloc[i,1], books[df.iloc[i,2]-1], ixv.iloc[i,4], ixv.iloc[i,2], df.iloc[i,3], ixv.iloc[i,5], ixv.iloc[i,0], df.iloc[i,4], ixv.iloc[i,3], df.iloc[i,0], books[df.iloc[i,2]-1], df.iloc[i,5], df.iloc[i,6], df.iloc[i,1], df.iloc[i,7].replace("LORD", """<span style="font-variant:small-caps;font-size:114%;">lord</span>"""), words[df.iloc[i,1]], ixv.iloc[i,0], ixv.iloc[i,3], glosstr0.iloc[i,7], ixv.iloc[i,0], ixv.iloc[i,3], groupedbyverse.iloc[i], ixv.iloc[i,0], ixv.iloc[i,3] ) )
+   
+   
+    currentFilename = books[df.iloc[i]["book"]-1].replace(" ","")+"."+str(df.iloc[i]["chapter"])+"."+str(df.iloc[i]["verse"])
+    currentContent  = verseTemplate.format(books[df.iloc[i]["book"]-1], df.iloc[i]["chapter"], df.iloc[i]["verse"], df.iloc[i]["trtext"], ixv.iloc[i]["previous_book"], books[df.iloc[i]["book"]-1], ixv.iloc[i]["next_book"], ixv.iloc[i]["previous_chapter"], df.iloc[i]["chapter"], ixv.iloc[i]["next_chapter"], ixv.iloc[i]["previous_verse"], df.iloc[i]["verse"], ixv.iloc[i]["next_verse"], df.iloc[i]["hebtext"], books[df.iloc[i]["book"]-1], df.iloc[i]["trchapter"], df.iloc[i]["trverse"], df.iloc[i]["number"], df.iloc[i]["trtext"].replace("LORD", """<span style="font-variant:small-caps;font-size:114%;">lord</span>"""), words[df.iloc[i]["number"]], ixv.iloc[i]["previous_verse"], ixv.iloc[i]["next_verse"], glosstr0.iloc[i]["generated"], ixv.iloc[i]["previous_verse"], ixv.iloc[i]["next_verse"], groupedbyverse.iloc[i], ixv.iloc[i]["previous_verse"], ixv.iloc[i]["next_verse"] ) 
+    currentRedirectionFilename = str(df.iloc[i]["number"])
+    currentRedirectionContent = """<html><head><meta http-equiv="refresh" content="0; URL=/v/"""+books[df.iloc[i]["book"]-1].replace(" ","")+"."+str(df.iloc[i]["chapter"])+"."+str(df.iloc[i]["verse"])+""".html"></head></html>"""
+    with open("v/"+currentFilename+".html","w+") as fout:
+        fout.write(currentContent)
+
 
 #1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5 ))
 
-    with open("v/"+str(df.iloc[i,1])+".html","w+") as fout2:
-        fout2.write("""<html><head><meta http-equiv="refresh" content="0; URL=/v/"""+books[df.iloc[i,2]-1].replace(" ","")+"."+str(df.iloc[i,3])+"."+str(df.iloc[i,4])+""".html"></head></html>""")
+    with open("v/"+currentRedirectionFilename+".html","w+") as fout2:
+        fout2.write(currentRedirectionContent)
 
-print(" ".join(list(set("".join(list(dfwt))))))
+#prints the alphabet of used symbols
+#print(" ".join(list(set("".join(list(dfwt))))))
 
 
 
